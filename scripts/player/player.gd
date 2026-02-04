4309fc260b0656f570dd469d58c88c7d1636803f
Xtrrae <131335730+Xtrrae@users.noreply.github.com>
2026-02-04 13:41:06 -0700

    added particles to the fireball

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody3D
 @onready var cam: Camera3D = $cam
 @onready var main = self.get_parent()
 @onready var projectile = load("res://scenes/fireball.tscn")
+@onready var pivot_center: Node3D = $pivot_center
+@onready var pivot_forward: Node3D = $pivot_center/pivot_forward
 
 var last_direction
 var lerp_speed = 0.08
@@ -16,6 +18,7 @@ var speed = 5.0
 var on_ground = true
 var on_box = false
 var direction : Vector3
+var current_rot : float
 
 const BASE_SPEED = 5.0
 const JUMP_VELOCITY = 6.0
@@ -27,7 +30,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	on_ground = is_on_floor()
 	# Add the gravity.
-	if not is_on_floor():
+	if not (on_box or on_ground):
 		
 		if Input.is_action_just_released("jump"):
 			velocity = lerp(velocity, get_gravity() * delta * 50, 0.3)
@@ -45,8 +48,9 @@ func _physics_process(delta: float) -> void:
 	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction and Input.is_action_pressed("sprint") and pushing == false:
 		direction = lerp(last_direction, direction, turn_speed)
-		gnome.rotation.y = lerp_angle(gnome.rotation.y, atan2(velocity.x, velocity.z), lerp_speed)
-
+		current_rot = lerp_angle(gnome.rotation.y, atan2(velocity.x, velocity.z), lerp_speed)
+		gnome.rotation.y = current_rot
+		pivot_center.rotation.y = current_rot
 		velocity.x = direction.x * sprint_speed
 		velocity.z = direction.z * sprint_speed
 		
@@ -54,24 +58,25 @@ func _physics_process(delta: float) -> void:
 	elif direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
-		gnome.rotation.y = lerp_angle(gnome.rotation.y, atan2(velocity.x, velocity.z), lerp_speed)
-		
+		current_rot = lerp_angle(gnome.rotation.y, atan2(velocity.x, velocity.z), lerp_speed)
+		gnome.rotation.y = current_rot
+		pivot_center.rotation.y = current_rot
 		last_direction = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 	
 	
-	"monitoring"
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
 	move_and_slide()
 
 func shoot():
 	
 	var instance = projectile.instantiate()
-	instance.dir = rotation
-	instance.spawnPos = self.global_position
-	instance.spawnRot.x = gnome.rotation.x
-	instance.spawnRot.z = gnome.rotation.z
+	instance.spawnPos = pivot_forward.global_position
+	instance.spawnRot = current_rot
 	main.add_child.call_deferred(instance)
 
 
@@ -88,3 +93,14 @@ func _on_area_3d_body_exited(body: Node3D) -> void:
 		body.collision_mask = 2
 		body.collision_layer = 2
 		on_box = false
+
+func _on_push_body_entered(body: Node3D) -> void:
+	if body.is_in_group("push"):
+		speed = 4.0
+		pushing = true
+
+
+func _on_push_body_exited(body: Node3D) -> void:
+	if body.is_in_group("push"):
+		speed = 5.0
+		pushing = false
