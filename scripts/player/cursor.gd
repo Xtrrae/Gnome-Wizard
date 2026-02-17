6bcb9d1d839b5401484c4672b9935302698606df
Xtrrae <131335730+Xtrrae@users.noreply.github.com>
2026-02-17 13:36:40 -0700

    snowball projectile added

--- scripts/player/cursor.gd
+++ scripts/player/cursor.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position = lerp(global_position, get_global_mouse_position(), 32.5*delta)
+	global_position = lerp(global_position, get_global_mouse_position(), 60*delta)
